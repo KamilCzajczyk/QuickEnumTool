be0b3aa61ddb4f13a658d068f3e28745a52fe129
KamilCzajczyk <k.czajczyk@outlook.com>
2024-10-27 20:53:17 +0100

    basic scanning

--- quickenum/cli.py
+++ quickenum/cli.py
@@ -1,1 +1,25 @@
-print(1+1)
+import nmap
+import subprocess
+
+def scanhost(host):
+    nm = nmap.PortScanner()
+    #default scan -> nmap -oX -sV <IP>
+    nm.scan(host, arguments="-sV")
+    print(f"Command used for scan: {nm.command_line()}")
+
+
+
+def main():
+    print("Welcome to QuickScan")
+    ip = input("Input IP Address: ")
+    scanhost(ip)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
